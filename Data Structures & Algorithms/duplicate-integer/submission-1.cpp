46d2f68fb7af46c58a05d0c5e051d9750050fed4
Shubham Raikar <165125316+raikarshubham@users.noreply.github.com>
2026-04-02 02:27:30 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& num) 
+    {
+        sort(num.begin(),num.end());
+        for(int i=0; i<num.size(); i++){
+            if(num[i]==num[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
