81af87ff6d1122ad21273fd87a50c0339d717748
Shubham Raikar <165125316+raikarshubham@users.noreply.github.com>
2026-07-02 16:58:33 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int XOR = 0;
+
+        for (int num : nums) {
+            XOR = XOR ^ num;
+        }
+
+        return XOR;
+    }
+};
