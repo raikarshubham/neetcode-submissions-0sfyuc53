0f80c484bc767c9ba228c20caa7de730cae4e04e
Shubham Raikar <165125316+raikarshubham@users.noreply.github.com>
2026-07-27 02:52:47 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        long long N = n;   // avoid overflow for INT_MIN
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1;
+
+        // for (long long i = 0; i < N; i++) {
+        //     ans *= x;
+        // }
+
+         while (N > 0) {
+
+            if (N % 2 == 1)
+                ans *= x;
+
+            x *= x;
+            N /= 2;
+        }
+
+        return ans;
+    }
+};
